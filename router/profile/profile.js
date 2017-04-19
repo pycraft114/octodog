bd88310a6057586540df0f78dcc2e3fd22d849c7
NEW <aufcl4858@naver.com>
2017-04-19 23:25:45 +0900

    Update left, right section rendering
    1. left-section 랜더링
    - 랜더링을 위한 템플릿 제작
    - 데이터베이스와 연동 완료, email, id, play, rank, topscore, totalscore 값 받아옴
    
    2. right-section 랜더링
    - canvas 삽입
    - Ajax 요청 후 받아온 데이터값 update 구현 완료

--- router/profile/profile.js
+++ router/profile/profile.js
@@ -5,6 +5,8 @@ var path = require('path');
 var options = require('../option');
 var mysql = require('mysql');
 
+var id = "'ma'";
+
 var loginData = {
   host: options.storageConfig.HOST,
   user: options.storageConfig.user,
@@ -27,29 +29,43 @@ router.get('/', function(req, res) {
 
 router.post('/user', function(req, res){
   var responseData = {};
-  var query = 'select `email`,`id`,`img` from user where id="ma";' +
-      'select score from scoreboard where uid="ma" ORDER BY num DESC limit 5;' +
-      'select count(*) from scoreboard where score=125;';
+  var temp;
+  var query = "select `email`,`id`,`img` from user where id=" + id + ";" +
+      "select score from scoreboard where uid=" + id + " ORDER BY num DESC limit 5;" +
+      "select count(*) from scoreboard where uid=" + id + ";" +
+      "select `score`,(select count(*)+1 from scoreboard where score>t.score) AS rank from scoreboard AS t where `uid`="+ id + " ORDER BY rank asc limit 1;";
 
   connection.query(query, function(err,rows){
+    // user 정보 - email, id, img, play, rank, topscore, totalscore
     responseData = {
-      result:"",
       user:{},
       chartscore:[]
     };
     if(err) throw err;
 
     // user 데이터 처리
-    responseData.user =  rows[0];
+    responseData.user =  rows[0][0];
 
     // chart에 사용될 score 데이터 처리
     rows[1].forEach(function(val){
       responseData.chartscore.push(val.score);
     })
+
+    // 플레이 횟수 처리
+    temp = rows[2][0]
+    responseData.user.play = temp["count(*)"];
+
+    // 현재 까지 모은 총 score 처리
     var sum = responseData.chartscore.reduce(function(a,b){
       return a+b;
     })
-    console.log(sum);
+    responseData.user.totalscore = sum;
+
+    // 랭크 처리
+    temp = rows[3][0];
+    responseData.user.rank = temp.rank;
+    responseData.user.topscore = temp.score;
+
 
     res.json(responseData);
   });
