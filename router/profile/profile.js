440859c5365f4c85f2b5f5d655ba72ebde4a15e3
NEW <aufcl4858@naver.com>
2017-04-19 22:13:45 +0900

    Updadte profile, implement total score query
    1. 총 포인트를 계산하는 커리 구현
    2. 현재까지 모은 총 포인트를 저장

--- router/profile/profile.js
+++ router/profile/profile.js
@@ -28,36 +28,28 @@ router.get('/', function(req, res) {
 router.post('/user', function(req, res){
   var responseData = {};
   var query = 'select `email`,`id`,`img` from user where id="ma";' +
-      'select score from scoreboard where uid="ma" ORDER BY num DESC limit 5;';
+      'select score from scoreboard where uid="ma" ORDER BY num DESC limit 5;' +
+      'select count(*) from scoreboard where score=125;';
 
   connection.query(query, function(err,rows){
     responseData = {
+      result:"",
       user:{},
       chartscore:[]
     };
     if(err) throw err;
 
     // user 데이터 처리
-    if(rows[0]){
-      responseData.result = "ok";
-      responseData.user =  rows[0];
-    }
-    else{
-      responseData.result = "none";
-      responseData.user = "";
-    }
+    responseData.user =  rows[0];
 
-    // chart에 사용될 score 처리
-    if(rows[1]){
-      responseData.result = "ok";
-      rows[1].forEach(function(val){
-        responseData.chartscore.push(val.score);
-      })
-    }
-    else{
-      responseData.result = "none";
-      responseData.chartscore = "";
-    }
+    // chart에 사용될 score 데이터 처리
+    rows[1].forEach(function(val){
+      responseData.chartscore.push(val.score);
+    })
+    var sum = responseData.chartscore.reduce(function(a,b){
+      return a+b;
+    })
+    console.log(sum);
 
     res.json(responseData);
   });
