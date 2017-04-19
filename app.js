87cea6cb710e5f2cfa6aeae9e0f7c54e050a35d0
NEW <aufcl4858@naver.com>
2017-04-19 17:13:42 +0900

    Add comment
    1. 주석 추가
    - index.js, main.html, option.js, app.js 등에 대한 주석 추가

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var router = require("./router/index")
+var router = require("./router/index")  // 라우팅은 middleware index이 담당하도록 권한을 위임한다.
 
-app.use(express.static('public'));
+app.use(express.static('public'));      // 정적 파일 경로 지정
 app.use(router);
 
 app.listen(3000,function(){
--- router/option.js
+++ router/option.js
@@ -1,3 +1,13 @@
+/*
+* 데이터 베이스의 개인 정보를 가져오는 파일 입니다.
+* git에 데이터베이스 비밀번호가 올라가면 안되므로 각자 config.json을 만들어주도록 합니다.
+* router 폴더 내부에 config.json파일을 만들어
+* HOST, user, password 3개의 값을 json 형식으로 입력합니다.
+* 이렇게 json 형식으로 받아온 파일을 사용하기 위해서는 js 폴더내부에 require시켜줍니다
+* 예) profile 폴더 -> var options = require('../option');
+* config.json은 gitignore에 추가 했으므로 별도의 조작은 필요하지 않습니다.
+*/
+
 var fs = require('fs'),
 configPath = __dirname + '/config.json';
 var parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
