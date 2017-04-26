ea15a19b5fb6dfec4b87c46dbc2d6de142c5174b
NEW <aufcl4858@naver.com>
2017-04-26 12:36:52 +0900

    Update Chart view class
    1. chart view 생성자 함수 완성

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,74 +1,69 @@
-function Profile(){
-  //chart dom
-  this.ctx = document.getElementById("myChart").getContext('2d');
-  // modal dom
-  this.modal = $('#myModal');
-  this.input = $(".input-text")
-  this.modalPw = $("#Mpw");
-  this.modalChangePw = $("#Mrepw");
-  this.modalChangePwConfirm = $("#Mrerepw");
-  this.warning = $(".warning");
+// after loaded event trigger
+document.addEventListener("DOMContentLoaded", function(){
 
-  function $(element){
-    return document.querySelector(element);
-  }
+function $(element){
+  return document.querySelector(element);
 }
 
-var profile =  new Profile();
+function ChartView(){
+  this.ctx = $("#myChart").getContext('2d');
 
-// chart rendering set object
-var data = {
-    labels: [
-        "", "", "", "", "recent"
-    ],
-    datasets: [
-        {
-            label: 'Your Score',
-            data: [
-                0, 0, 0, 0, 0
-            ],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }
-    ]
-};
+  // chart rendering set object
+  this.data = {
+      labels: [
+          "", "", "", "", "recent"
+      ],
+      datasets: [
+          {
+              label: 'Your Score',
+              data: [
+                  0, 0, 0, 0, 0
+              ],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }
+      ]
+  };
 
-var options = {
-    animation: {
-        animateScale: true
-    },
-    responsive: false,
-    scales: {
-        yAxes: [
-            {
-                ticks: {
-                    beginAtZero: true
-                }
-            }
-        ]
-    }
-};
+  this.options = {
+      animation: {
+          animateScale: true
+      },
+      responsive: false,
+      scales: {
+          yAxes: [
+              {
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }
+          ]
+      }
+  };
+}
 
-var ctx = document.getElementById("myChart").getContext('2d');
-var myBarChart = new Chart(ctx, {
+var chartView =  new ChartView()
+
+
+var myBarChart = new Chart(chartView.ctx, {
     type: 'bar',
-    data: data,
-    options: options
+    data: chartView.data,
+    options: chartView.options
 });
 
 // modal part
@@ -164,13 +159,13 @@ function leftSideRender(resultData) {
 // right side rendering function
 function rightSideRender(resultData) {
     var score = resultData.chartscore.reverse();
-    var comp_data = data.datasets[0].data;
+    var comp_data = chartView.data.datasets[0].data;
 
     for (var i = 0; i < comp_data.length; i++) {
         comp_data[i] = score[i];
     }
 
-    data.datasets[0].data = comp_data;
+    chartView.data.datasets[0].data = comp_data;
     myBarChart.update();
 }
 
@@ -200,7 +195,6 @@ for(var i = 0; i < input.length; i++){
   })
 }
 
-// after loaded event trigger
-document.addEventListener("DOMContentLoaded", function(){
+
     sendAjax('http://localhost:3000/profile/user', "init");
 });
