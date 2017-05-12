fa7423680d9d846282ba11f9d40bc4972b9f2520
pycraft114 <pycraft114@gmail.com>
2017-05-12 10:48:46 +0900

    Facade patter code

--- public/js/fortest.js
+++ public/js/fortest.js
@@ -1,14 +1,16 @@
-function Page(objectContent){
-    for(let key in objectContent){
-        this[key] = objectContent[key]
+function merge(arr1,arr2){
+    let length;
+    let returnArray = [];
+    if(arr1.length > arr2.length){length = arr1.length}
+    else{length = arr2.length}
+    for(let i = 0; i < length; i ++){
+        returnArray.push(returnSmall(arr1[i],arr2[i]))
     }
 }
 
-const smth = {
-    that : this,
-    hi : "this is hi"
-};
+function returnSmall(num1,num2){
+    if(num1 >= num2){return num2}
+    else{return num1}
+}
 
-var test = new Page(smth);
-
-console.log(test);
+console.log((2+3)/2);
--- public/js/loginPage_Refactoring.js
+++ public/js/loginPage_Refactoring.js
@@ -112,54 +112,29 @@ const octoDog = function(){
             loginSuccess: "<li>로그인 완료.</li>"
         },
 
-        verifier : function(responseType){
+        verifier : function(responseText){
+            console.log(this);
+            const self = this;
             const cases = {
                 "user not found" : function(){
-                    this.changeAttribute(this.warningListNode, "innerHTML", this.warningMessage.noUser);
+                    self.changeAttribute(self.warningListNode, "innerHTML", self.warningMessage.noUser);
                 },
                 "incorrect password" : function(){
-                    this.changeAttribute(this.warningListNode, "innerHTML", this.warningMessage.wrongPassword);
+                    self.changeAttribute(self.warningListNode, "innerHTML", self.warningMessage.wrongPassword);
                 },
                 "login success" : function(){
-                    this.changeAttribute(this.warningListNode, "innerHTML", this.warningMessage.loginSuccess);
+                    self.changeAttribute(this.warningListNode, "innerHTML", self.warningMessage.loginSuccess);
                 },
                 default : function(){
                     console.log("login page verifier called");
                 }
             };
-            (cases[responseType].bind(this) || cases["default"])();
+            (cases[responseText] || cases["default"])();
         }
     };
 
     //initiate loginPage
     const loginPage = new Page(loginPageContent);
-
-
-    loginPage.signUpButton.addEventListener("click", function(){
-        this.changeAttribute(this.modal,"className","on");
-        this.changeAttribute(this.modalContent,"className","on");
-    }.bind(loginPage));
-
-    loginPage.modal.addEventListener("click",function(evt){
-        if(evt.target === this.modal){
-            this.changeAttribute(this.modal,"className","off");
-            this.changeAttribute(this.modalContent,"className","off");
-        }
-    }.bind(loginPage));
-
-    loginPage.loginButton.addEventListener("click",function(evt){
-        if(this.checkEmptyInput([this.loginId, this.loginPassword])){
-            this.changeAttribute(this.warningListNode, "innerHTML", this.warningMessage.noContent);
-        }else {
-            const data = {};
-            data['id'] = this.loginId.value;
-            data['password'] = this.loginPassword.value;
-            sendAjax("POST","/login",data,"application/json", function() {
-                loginPage.ajaxResponseHandler(loginPage.verifier.bind(loginPage), this.responseText);
-                                                                //bind안하면 verifier함수내의 this가 window를 가르킴
-            })
-        }
-    }.bind(loginPage));
     //-------------------------------------------------------
 
     //------------------------modal---------------------------
@@ -177,7 +152,7 @@ const octoDog = function(){
             emailInUse: "<li>이미 사용중인 이메일 입니다.</li>"
         },
 
-        verifier : function(responseType){
+        verifier : function(responseText){
             const cases = {
                 "id in use" : function(){
                     this.changeAttribute(this.warningListNode, "innerHTML", this.warningMessage.idInUse);
@@ -193,13 +168,43 @@ const octoDog = function(){
                     console.log("modal verifier called");
                 }
             };
-            (cases[responseType].bind(this) || cases["default"])();
+            (cases[responseText].bind(this) || cases["default"])();
         }
     };
 
     //initiate modal
     const modal = new Page(modalContent);
 
+
+    //---------------------------------add events-----------------------------
+
+    
+    loginPage.signUpButton.addEventListener("click", function(){
+        this.changeAttribute(this.modal,"className","on");
+        this.changeAttribute(this.modalContent,"className","on");
+    }.bind(loginPage));
+
+    loginPage.modal.addEventListener("click",function(evt){
+        if(evt.target === this.modal){
+            this.changeAttribute(this.modal,"className","off");
+            this.changeAttribute(this.modalContent,"className","off");
+        }
+    }.bind(loginPage));
+
+    loginPage.loginButton.addEventListener("click",function(evt){
+        //checkemptyinput this 없이 부를수있는방법없나 클로저라서 사라지는경우말고 가져다쓸수있게
+        if(this.checkEmptyInput([this.loginId, this.loginPassword])){
+            this.changeAttribute(this.warningListNode, "innerHTML", this.warningMessage.noContent);
+        }else {
+            const data = {};
+            data['id'] = this.loginId.value;
+            data['password'] = this.loginPassword.value;
+            sendAjax("POST","/login",data,"application/json", function() {
+                loginPage.ajaxResponseHandler(loginPage.verifier, this.responseText);
+                //bind안하면 verifier함수내의 this가 window를 가르킴
+            })
+        }
+    }.bind(loginPage));
 
 
     modal.submitButton.addEventListener("click",function(evt){
@@ -215,6 +220,15 @@ const octoDog = function(){
 
             sendAjax('POST','/signup',data,'application/json',function(){
                 modal.ajaxResponseHandler(modal.verifier.bind(modal), this.responseText);
+                //단순 warning List node inner html 바꾸는 역할 하는 함수랑
+                //실질적으로 로그인이나 회원가압 성공했을때 어떤 기능을하는 함수랑 분리
+                // 예를 들면 if(this.responseText !== "success") {
+                //  modal.changeAttribute(modal.warningListNode, "innerHTML", "<li>"+this.responseText+"</li>");
+                // }
+                //else 일때는 성공했을때이니 굳이 verifier가 필요없을것같음
+                //그런데 이렇게 했을때의 문제점은 success가아닌 각자 개별 상황마다 단순히 warningListNode만 바꾸는게 아니고 다른 기능들을 할때는
+                //적용을 할수가없음
+                //
             })
         }
     }.bind(modal));
@@ -256,4 +270,119 @@ case "이메일 사용중":
 })
 */
 
+const octo = {};
 
+(function(octo) {
+    function changeAttribute(element, attribute, value){
+        element[attribute] = value;
+    }
+
+    function isEmpty(element){
+        if(element.value.length === 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function $(selector){
+        return document.querySelector(selector);
+    }
+
+
+
+    function SubmitPageElement(page, selector) {
+        this.page = page;
+        this.element = $(selector);
+    }
+
+    SubmitPageElement.prototype.onClick = function(clickHandler) {
+      this.element.addEventListener("click", clickHandler);
+    };
+
+    SubmitPageElement.prototype.addEvent = function(name, eventHandler) {
+        const event = new Event(name);
+
+        this.events[name] = event;
+        this.element.addEventListener(name, eventHandler, false);
+    };
+
+    SubmitPageElement.prototype.necessary = function(isConfirmation, sameSelector) {
+        this.page.necessaryInputs.push(this.element);
+
+        if(isConfirmation && this.page[sameSelector]) {
+            this.page.confirmationInputs.push(this.element);
+            this.page.confirmationInputs.push(this.page[sameSelector]);
+        }
+    };
+
+    SubmitPageElement.prototype.warningNode = function() {
+        this.page.warningNode = this.element;
+    };
+
+    SubmitPageElement.prototype.attachSubmit = function(method, url, responseHandler) {
+        const page = this.page;
+
+        this.element.addEventListener("click", function() {
+            for (const necessaryInput of page.necessaryInputs) {
+                if(isEmpty(necessaryInput)) {
+                    page.showError("No Content")
+                }
+            }
+
+            if ((page.confirmationInputs);
+
+        });
+    };
+
+
+
+    octo.SubmitPage = function(name) {
+        this.name = name;
+        this.necessaryInputs = [];
+        this.confirmationInputs = [];
+        this.warningElement = null;
+    };
+
+    SubmitPage.init = function(name) {
+        return new SubmitPage(name);
+    };
+
+    SubmitPage.prototype.addElement = function(name, selector) {
+        this[name] = new SubmitPageElement(this, selector);
+
+        return this[name];
+    };
+
+    SubmitPage.prototype.addWarningElement = function(selector) {
+        this.warningElement = $(selector);
+    };
+
+    SubmitPage.prototype.showError = function(message) {
+        changeAttribute(this.warningElement, "innerHTML", message)
+    }
+
+
+})(octo);
+
+
+
+(function() {
+    const modal = octo.SubmitPage.init('loginPage');
+    modal.addElement('signUpPassword', '#signup-password')
+        .necessary();
+    modal.addElement('signUpConfirm', '#signup-confirm')
+        .necessary(true, 'signUpPassword');
+
+    modal.addElement('signUpId', '#signup-id')
+        .necessary();
+
+    modal.addElement('signUpEmail', '#signup-email')
+        .necessary();
+
+    modal.addWarningElement('warningListNode', '#modal-warning ul');
+
+    modal.addElement('submitButton', '#submit')
+        .attachSubmit(url, );
+
+})()
